7cbb6cdedbdf45f3c2a55f168131f3027a761530
Fernanda Vidal <fevidal.dev@gmail.com>
2022-11-14 20:14:30 -0300

    feat: implementa TransferService e IPayment.

---
+++ src/Services/TransferService.ts
@@ -0,0 +1,17 @@
+import Payment from '../Domain/Payment';
+
+class TransferService {
+  private isValidKey(key: string): boolean {
+    const cpfRegex = /^\d{3}.\d{3}.\d{3}-\d{2}$/;
+    return cpfRegex.test(key);
+  }
+
+  public async transfer(payment: IPayment) {
+    if (!this.isValidKey(payment.key)) throw new Error('Invalid Key!');
+    // Criar instância da Model de Payment usando Mongoose
+    // Inserir os dados no banco
+    // Retornar os dados com o id
+  }
+}
+
+export default TransferService;
