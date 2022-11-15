c9692d7b17b555e240bdce0ead756171befeabed
Fernanda Vidal <fevidal.dev@gmail.com>
2022-11-15 14:16:28 -0300

    feat: Implementa estrutura express.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
 // Rotas e middlewares devem ficar aqui
+import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import routes from './Routes/Routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(ErrorHandler.handle);
+
+export default app;
